8139788a176533c6fb272cfbf15f539f8b150923
emmkiprono-coder <emmkiprono@gmail.com>
2026-03-09 10:50:02 -0500

    Delete src directory

--- src/hooks/use-toast.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from "react"
-export function useToast() {
-  const [toasts, setToasts] = useState<any[]>([])
-  return { toasts, toast: (props: any) => setToasts(p => [...p, props]), dismiss: () => setToasts([]) }
-}
-export type { useToast }
